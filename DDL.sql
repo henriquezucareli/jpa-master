a0194185ea04fe1e23cca97a0693df6e5097fd2d
henriquezucareli <henrique.zucareli@gmail.com>
2020-09-21 23:09:07 +0000

    Atualização arquivos sql 21-09 20:09

--- DDL.sql
+++ DDL.sql
@@ -7,43 +7,43 @@ create user 'user'@'localhost' identified by 'pass123';
 grant select, insert, delete, update on nba_organization.* to user@'localhost';
 
 create table tea_team (
-    tea_id bigint unsigned primary key auto_increment,
+    id bigint unsigned primary key auto_increment,
     tea_city varchar(30) not null,
     tea_name varchar(30) not null,
     tea_conference varchar (4) not null
 );
 
 create table ros_roster (
-  ros_id bigint unsigned primary key auto_increment,
-  ros_first_name varchar(50) not null,
-  ros_last_name varchar(50) not null,
-  ros_salary float not null,
-  ros_born datetime not null,
-  ros_team bigint,
-  constraint ros_tea_fk foreign key (ros_team)
-    references tea_team (tea_id)
+    id bigint unsigned primary key auto_increment,
+    ros_first_name varchar(50) not null,
+    ros_last_name varchar(50) not null,
+    ros_salary float not null,
+    ros_born datetime not null,
+    tea_id bigint,
+    constraint ros_tea_fk foreign key (tea_id)
+        references tea_team (id)
 );
 
 create table pla_player (
-  pla_id bigint unsigned primary key,
-  pla_position varchar(15) not null,
-  constraint pla_ros_fk foreign key (pla_id)
-    references ros_roster (ros_id)
+    id bigint unsigned primary key,
+    pla_position varchar(15) not null,
+    constraint pla_ros_fk foreign key (id)
+        references ros_roster (id)
 );
 
 create table sta_staff (
-  sta_id bigint unsigned primary key,
-  sta_function varchar(30) not null,
-  constraint sta_ros_fk foreign key (sta_id)
-    references ros_roster (ros_id)
+    id bigint unsigned primary key,
+    sta_function varchar(30) not null,
+    constraint sta_ros_fk foreign key (id)
+        references ros_roster (id)
 );
 
 create table pla_historic (
     pla_id bigint primary key,
     tea_id bigint primary key,
     constraint pla_his_fk foreign key (pla_id)
-        references pla_player (pla_id)
+        references pla_player (id),
     constraint tea_his_fk foreign key (tea_id)
-        references tea_team (tea_id)
-)
+        references tea_team (id)
+);
 
