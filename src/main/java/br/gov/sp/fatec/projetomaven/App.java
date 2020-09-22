2ac6adc9b8199cbd5bba118d0d7f309acde31bc7
henriquezucareli <henrique.zucareli@gmail.com>
2020-09-22 23:25:50 +0000

    Finalizado o Java App 22-09 20:25

--- src/main/java/br/gov/sp/fatec/projetomaven/App.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/App.java
@@ -1,123 +1,54 @@
 package br.gov.sp.fatec.projetomaven;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceException;
-import javax.persistence.TypedQuery;
-
-import br.gov.sp.fatec.projetomaven.dao.PersistenceManager;
+import br.gov.sp.fatec.projetomaven.dao.PlayerDao;
+import br.gov.sp.fatec.projetomaven.dao.PlayerDaoJpa;
+import br.gov.sp.fatec.projetomaven.dao.StaffDao;
+import br.gov.sp.fatec.projetomaven.dao.StaffDaoJpa;
+import br.gov.sp.fatec.projetomaven.dao.TeamDao;
+import br.gov.sp.fatec.projetomaven.dao.TeamDaoJpa;
 import br.gov.sp.fatec.projetomaven.entity.Player;
 import br.gov.sp.fatec.projetomaven.entity.Staff;
 import br.gov.sp.fatec.projetomaven.entity.Team;
 import br.gov.sp.fatec.projetomaven.entity.enums.ConferenceEnum;
+import br.gov.sp.fatec.projetomaven.entity.enums.PositionEnum;
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-        EntityManager manager = PersistenceManager.getInstance().getEntityManager();
-     
-        // Instancia um novo staff
-        Staff staff = new Staff();
-        staff.setRosterFirstName("Gregg");
-        staff.setRosterLastName("Popovich");
-        staff.setRosterBorn(new Date(28/01/1949));
-        staff.setRosterSalary(20000);
-        staff.setStaffFunction("Coach");
-        // Instancia uma nova equipe
-        Team team = new Team();
-        team.setTeamConference(ConferenceEnum.WEST);
-        team.setTeamCity("SAN_ANTONIO");
-        team.setTeamName("SPURS");
-        team.setId(1L);
-        // Vincula staff à equipe
-        staff.setRosterTeam(team);
+        TeamDao teamDao = new TeamDaoJpa();
+        PlayerDao playerDao = new PlayerDaoJpa();
+        StaffDao staffDao = new StaffDaoJpa();
 
 
+        //Registra Times
+        Team mia = teamDao.registerTeam("Miami", "Heat", ConferenceEnum.EAST);
+        Team sas = teamDao.registerTeam("San Antonio", "Spurs", ConferenceEnum.WEST);
+        Team phx = teamDao.registerTeam("Phoenix", "Suns", ConferenceEnum.WEST);
 
-
-        // Instancia um novo Jogador
-        Player player = new player(create);
-        trabalho.setTitulo("Trabalho 2 - JPA");
-        trabalho.setDataHoraEntrega(new Date());
-        trabalho.setLocalArquivo("trabalhos");
-        // Associa o professor ao trabalho como avaliador
-        trabalho.setAvaliador(professor);
-        // Associa o aluno ao trabalho
-        trabalho.setAlunos(new HashSet<Aluno>());
-        trabalho.getAlunos().add(aluno);
-
-        // Salva aluno, professor e trabalho
-        TrabalhoDao trabalhoDao = new TrabalhoDaoJpa(manager);
-        trabalhoDao.salvarTrabalho(trabalho);
-        // Após o salvamento os IDs estarão preenchidos
-
-        // Limpa o cache para forçar a execução de SELECT
-        manager.clear();
-
-        // Busca o aluno pelo ID
-        aluno = manager.find(Aluno.class, aluno.getId());
-        System.out.println(aluno.getId());
-        System.out.println(aluno.getNomeUsuario());
-        for(Trabalho trab: aluno.getTrabalhos()) {
-            System.out.println(trab.getTitulo());
-        }
-
-        // Limpa o cache para forçar a execução de SELECT
-        manager.clear();
+        //Registra Jogadores
+        Player herro = playerDao.registerPlayer("Tyler", "Herro", PositionEnum.SHOOTING_GUARD, 3640200f, new Date(20/1/2000), mia);
+        Player butler = playerDao.registerPlayer("Jimmy", "Butler", PositionEnum.SMALL_FORWARD, 32740000f, new Date(14/9/1989), mia);
+        Player adebayo = playerDao.registerPlayer("Edrice", "Adebayo", PositionEnum.CENTER, 3454080f, new Date(18/7/1997), mia);
+        Player murray = playerDao.registerPlayer("Dejounte", "Murray", PositionEnum.POINT_GUARD, 2321735f, new Date(19/7/1996), sas);
         
-        // Busca o trabalho pelo ID
-        trabalho = manager.find(Trabalho.class, trabalho.getId());
-        System.out.println(trabalho.getTitulo());
-        for(Aluno al: trabalho.getAlunos()) {
-            System.out.println(al.getNomeUsuario());
-        }
+        //Registra Staff
+        Staff pop = staffDao.registerStaff("Gregg", "Popovich", "Head Coach", 8000000f, new Date(28/1/1949), sas);
+        Staff duncan = staffDao.registerStaff("Tim", "Duncan", "Assistant Coach", 1000000f, new Date(25/4/1976), sas);
+        Staff thib = staffDao.registerStaff("Tom", "Thibodeau", "Head Coach", 0f, new Date(17/1/1958), null);
 
-        trabalho.setTitulo("Novo Trabalho 2 - JPA");
-        
-        try {
-            manager.getTransaction().begin();
-            manager.merge(trabalho);
-            manager.getTransaction().commit();
-        }
-        catch(PersistenceException e) {
-            e.printStackTrace();
-            manager.getTransaction().rollback();
-        }
 
-        String queryString = "select t from Trabalho t inner join t.alunos a where a.nomeUsuario like :nome";
-        TypedQuery<Trabalho> query = manager.createQuery(queryString, Trabalho.class);
-        query.setParameter("nome", "%bd%");
+        //Busca jogador por time
+        playerDao.searchPlayersByTeam (mia);
 
-        List<Trabalho> resultados = query.getResultList();
-        for(Trabalho trab: resultados) {
-            System.out.println("Título: " + trab.getTitulo());
-        }
+        //Busca Equipe por Conferência
+        teamDao.searchTeamsByConference (ConferenceEnum.WEST);
 
-        // Apaga registros (permite re-execução)
-        
-        try {
-            manager.getTransaction().begin();
-            professor = trabalho.getAvaliador();
-            trabalho.setAvaliador(null);
-            Set<Aluno> alunos = trabalho.getAlunos();
-            trabalho.setAlunos(null);
-            manager.remove(trabalho);
-            manager.remove(professor);
-            for(Aluno al: alunos) {
-                manager.remove(al);
-            }
-            manager.getTransaction().commit();
-        }
-        catch(PersistenceException e) {
-            e.printStackTrace();
-            manager.getTransaction().rollback();
-        }
+        //Busca Staff por time
+        staffDao.searchStaffsByTeam(sas);
 
-        manager.close();
     }
 }
